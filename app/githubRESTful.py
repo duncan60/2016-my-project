1984b072e908153565d6c23005154d733db426c7
duncan60 <tmduncan60@gmail.com>
2016-03-21 22:10:58 +0800

    feat(github-api): update github api

--- app/githubRESTful.py
+++ app/githubRESTful.py
@@ -20,7 +20,7 @@ class CallBack(Resource):
 
 		session['github_access_token'] = oauth_token
 		return {
-			'oauth_token': github_access_token
+			'msg': 'login successed !'
 		}
 
 class GetUser(Resource):
@@ -31,7 +31,17 @@ class Repos(Resource):
 	def get(self, login):
 		return github.get('users/' + login + '/repos')
 
+class Branchs(Resource):
+	def get(self, user, repo):
+		return github.get('repos/' + user + '/' + repo + '/branches')
+
+class TreeFile(Resource):
+	def get(self, user, repo, branch):
+		return github.get('repos/' + user + '/' + repo + '/branches/' + branch )
+
 api.add_resource(Login, '/github/login', endpoint = 'github-login')
 api.add_resource(CallBack, '/github/callback', endpoint = 'github-callback')
 api.add_resource(GetUser, '/github/user', endpoint = 'github-user')
 api.add_resource(Repos, '/github/repos/<login>', endpoint = 'github-repos')
+api.add_resource(Branchs, '/github/branchs/<user>/<repo>', endpoint = 'github-branch')
+api.add_resource(TreeFile, '/github/tree_file/<user>/<repo>/<branch>', endpoint = 'github-tree-file')
